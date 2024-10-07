6cd4bb4b4804ff5b8e71e8790ab8148113712e49
zarkasy <sereneberuhigen@gmail.com>
2024-10-07 15:45:40 +0700

    asdfadsf

--- belajar-baca/assets/assets/whack_a_mole/tiled/hit_feedback.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="hit_feedback" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="hit_feedback.png" width="96" height="96"/>
-</tileset>
