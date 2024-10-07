331f9c7dcb12f495cc6bf665c1712c03b36ddbf1
zarkasy <sereneberuhigen@gmail.com>
2024-10-07 16:27:33 +0700

    kynya error di subdomain aplikasi

---
+++ public/belajar-baca/assets/assets/urutkan_angka/tiles/Tower_Red.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tower_Red" tilewidth="64" tileheight="64" tilecount="8" columns="2">
+ <image source="../images/Tower/Tower_Red.png" width="128" height="256"/>
+</tileset>
