dde5f4807bb8c09868604d7543179ea78e577c5c
zarkasy <zarkasy@users.noreply.github.com>
2024-10-07 07:52:25 +0000

    deploy: 469ce1633c969c7485378cfd06c20b9c419a6e3d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -86,7 +86,7 @@ const RESOURCES = {"assets/AssetManifest.json": "1dcb14687ef025bbfbf05525f3b6d1b
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "1d432f097d44520e857cd76a9a0ede06",
+"flutter_bootstrap.js": "5271963fc36f363526f4e796e0b232d2",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "index.html": "da14b4cef87cc762a437ef8d5b3e9edb",
