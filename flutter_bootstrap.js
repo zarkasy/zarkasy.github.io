e6b0964447e559503d22b68b12ef7a883136efa9
zarkasy <zarkasy@users.noreply.github.com>
2024-10-28 03:59:28 +0000

    deploy: d6e8bbffc3e1ee2e9ae9fe586d8ebaa060075418

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3297913474"
+    serviceWorkerVersion: "566362967"
   }
 });
