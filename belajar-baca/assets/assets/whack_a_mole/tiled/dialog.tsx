8c63b59c3dbc19cfed409114999258eacc0d7ae8
zarkasy <sereneberuhigen@gmail.com>
2024-10-31 05:29:46 +0700

    salah td

--- belajar-baca/assets/assets/whack_a_mole/tiled/dialog.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="dialog" tilewidth="16" tileheight="16" tilecount="9" columns="3">
- <image source="dialog.png" width="48" height="48"/>
-</tileset>
