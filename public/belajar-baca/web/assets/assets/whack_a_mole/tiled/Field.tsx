00f299c5333e6031f56ac9ca60de6fcce8f64047
zarkasy <sereneberuhigen@gmail.com>
2024-10-31 10:17:06 +0700

    perbaiki salah copy di folder public

--- public/belajar-baca/web/assets/assets/whack_a_mole/tiled/Field.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Field" tilewidth="64" tileheight="64" tilecount="3" columns="1">
- <image source="../images/NinjaAdventure/Backgrounds/Tilesets/TilesetField.png" width="80" height="240"/>
-</tileset>
